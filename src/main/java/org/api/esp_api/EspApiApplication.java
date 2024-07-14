d6d2b9a5d5e4c2ed661dc3e9e81fb02b160df20d
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-07-14 23:57:02 +0300

    Initial commit

---
+++ src/main/java/org/api/esp_api/EspApiApplication.java
@@ -0,0 +1,13 @@
+package org.api.esp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspApiApplication.class, args);
+	}
+
+}
